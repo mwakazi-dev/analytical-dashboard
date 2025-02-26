9d583c49c29863982da44dd8bb60f0a066d3f0f8
mwakazi-cio <johnson.mwakazi@cioafrica.co>
2025-02-26 21:15:03 +0300

    fix: deployment

--- components/templates/layout/layout.stories.tsx
+++ components/templates/layout/layout.stories.tsx
@@ -1,11 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Layout from ".";
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
 
 const meta = {
   title: "Templates/Layout",
@@ -20,28 +15,11 @@ type Story = StoryObj<typeof meta>;
 
 export const DefaultLayout: Story = {
   args: {
-    menus: [
-      {
-        key: "1",
-        label: "Overview",
-        onClick: () => {
-          console.log("Home");
-        },
-        icon: <AppstoreOutlined />,
-      },
-      {
-        key: "2",
-        label: "Reports",
-        onClick: () => {},
-        icon: <BarChartOutlined />,
-      },
-      {
-        key: "3",
-        label: "Settings",
-        onClick: () => {},
-        icon: <SettingOutlined />,
-      },
-    ],
+    children: (
+      <div>
+        <p>Content</p>
+      </div>
+    ),
   },
 };
 
